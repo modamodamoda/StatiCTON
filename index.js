4627f47493521052ab27f7aa1500508d7a753b98
Jack Penny <apigeonabroad@Jacks-MacBook-Pro.local>
2020-01-24 15:12:36 +0800

    ADd middleware support

--- index.js
+++ index.js
@@ -5,8 +5,18 @@
 (4) Profit
 */
 
-var yaml = require('js-yaml'), showdown = require('showdown'), fs = require('fs');
-var converter = new showdown.Converter();
+const yaml = require('js-yaml'), showdown = require('showdown'), fs = require('fs'), path = require('path');
+const converter = new showdown.Converter();
+
+function requireCWD(fname) { // stole from SO to deal with middleware requires
+    var fullname = fname;
+    if (fname && fname.length && 
+      !path.isAbsolute(fname) &&
+      fname.charAt(0) !== '.') {
+        fullname = path.join(process.cwd(), fname);
+    }
+    return require(fullname);
+}
 
 // -- Walk --
 
@@ -66,7 +76,7 @@ class Template {
                 }
             }
             js? (code += type != '{' && (line.match(reExp) || line.match(reExp2)) ? line + '\n' : found == 1 ? 'r.push(' + line + ');\n' : 'r.push(' + line + ');\n' ) :
-                (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"').replace(/\n/g, '\\n') + '");\n' : '');
+                (code += line != '' ? 'r.push("' + line.replace(/\\/g, '\\\\').replace(/"/g, '\\"').replace(/\n/g, '\\n') + '");\n' : '');
             return add;
         }
         while(match = re.exec(html)) {
@@ -142,12 +152,21 @@ class Item {
 }
 // -- The actual stuff
 var Render = module.exports = class {
-    constructor(dir) {
+    async emitMiddleware(type, ...args) {
+        for(let i in this._middleware[type]) {
+            let r = this._middleware[type][i](...args); // run current function
+            if(r && typeof r.then === 'function') await r.then(); // if it's a promise, wait for it to complete
+        }
+        return;
+    }
+    constructor(dir, config = 'objects.yml') {
         this.dir = dir;
         this.config = {};
         this.items = {};
         this.attributes = {};
         this.output = {};
+        this.configFile = config;
+        this._middleware = { 'file': [], 'after': [] };
         this.globals = {
             route: (relative) => {
                 let base = (this.config.urlBase ? this.config.urlBase : '') + relative;
@@ -172,78 +191,106 @@ var Render = module.exports = class {
         };
         this.macros = [];
     }
+    // middleware(type, callback) adds a middleware
+    middleware(type, callback) {
+        if(!this._middleware[type]) this._middleware[type] = [];
+        this._middleware[type].push(callback);
+    }
     // loadConfig() loads the conifg
     loadConfig() {
         try {
-            let configData = fs.readFileSync(this.dir + '/objects.yml');
+            let configData = fs.readFileSync(this.dir + '/' + this.configFile);
             // this is the main yaml configuration
             this.config = yaml.safeLoad(configData.toString());
         } catch(e) {
             throw 'Error: objects file not found or is not valid YAML';
         }
+        // If there are middlewares available, include them
+        if(this.config.middleware) {
+            for(let type in this.config.middleware) {
+                for(let file of this.config.middleware[type]) {
+                    this.middleware(type, file.substr(0,2) == './' ? requireCWD(file.substr(2)) : require(file)); // for globals
+                }
+            }
+        }
     }
     // loadItems() loads the items
-    loadItems() {
+    async loadItems() {
         let allFiles = walkSync(this.dir);
         for(let i of allFiles) {
             // Process all files
             if(i.fileType == 'html' || i.fileType == 'md') {
                 // this is a layout
                 this.items[i.relativeName] = new Item(this, i.relativeName, fs.readFileSync(i.fileName), i.fileType);
+            } else if(i.fileType != 'yml') { // ignore our yml files
+                if(this._middleware['file:' + i.fileType]) {
+                    // this file is handled by a middleware
+                    await this.emitMiddleware('file:' + i.fileType, i.fileName, this.config.base + '/' + i.relativeName);
+                }
             }
         }
         // -- Process macros
-        for(let g of this.config.macros) {
-            if(g.attribute) {
-                // Attribute macro
-                // Purpose: iterates all available attributes in a page, and generates a page for each
-                for(let i in this.attributes[g.attribute]) {
-                    let href = g.href.replace('%value', i.toLowerCase());
-                    this.items[href] = new Item(this, href, '---\nparent: ' + g.layout + '\ntitle: ' + i + '\n---\n', 'html');
-                    this.items[href].config.local = this.attributes[g.attribute][i];
+        if(this.config.macros) {
+            for(let g of this.config.macros) {
+                if(g.attribute) {
+                    // Attribute macro
+                    // Purpose: iterates all available attributes in a page, and generates a page for each
+                    for(let i in this.attributes[g.attribute]) {
+                        let href = g.href.replace('%value', i.toLowerCase());
+                        this.items[href] = new Item(this, href, '---\nparent: ' + g.layout + '\ntitle: ' + i + '\n---\n', 'html');
+                        this.items[href].config.local = this.attributes[g.attribute][i];
+                    }
                 }
-            }
-            for(let i of this.macros) {
-                this.macros[i](g);
+                for(let i of this.macros) {
+                    this.macros[i](g);
+                }
             }
         }
     }
     // build(write = true/false) builds the items
     // write = false means that the rendered content will simply be rendered to memory in the this.output object
-    build(write = true) {
+    async build(write = true) {
         for(let g in this.items) {
             if(this.items[g].hasConfig && this.items[g].config.build != false) {
                 // render out this page
                 let dirs = this.items[g].href.split('/');
                 dirs.pop();
+                let page = {
+                    data: this.items[g].render(Object.assign( { pages: this.items }, this.globals)),
+                    config: this.items[g].config,
+                    globals:this.globals,
+                    name: g };
+                await this.emitMiddleware('after', page);
                 if(write) {
                     fs.mkdir(this.config.base + '/' + dirs.join('/'), { recursive: true }, (err) => {
                         if (err) throw err;
+                        fs.writeFileSync(this.config.base + '/' + this.items[g].href, page.data);
                     });
-                    fs.writeFileSync(this.config.base + '/' + this.items[g].href, this.items[g].render(Object.assign( { pages: this.items }, this.globals)));
                 } else {
-                    this.output[g] = this.items[g].render(Object.assign( { pages: this.items }, this.globals));
+                    this.output[g] = page.data;
                 }
             }
         }
+        this.emitMiddleware('finish');
     }
     // all does everything with no effort
-    all(write = true) {
-        this.loadConfig();
-        this.loadItems();
-        this.build(write);
+    async all(write = true) {
+        if(!this.config.base) this.loadConfig();
+        await this.loadItems();
+        await this.build(write);
     }
 }
 
 // Now for command line stuff
 if (require.main === module) {
-    let pth = process.argv.pop();
-    let opt = process.argv.pop();
     let buildDir;
-    if(opt == '-d') {
-        buildDir = pth;
-    } else {
-        buildDir = '.';
+    let opts = {};
+    let co;
+    if(process.argv && process.argv.length > 0) {
+       while(co = process.argv.shift()) {
+          if(co[0] == '-') opts[co.substr(1)] = process.argv.shift();
+       }
     }
-    new Render(buildDir).all();
+    if(opts['d']) process.chdir(opts['d']);
+    new Render('.', opts['c'] || 'objects.yml').all();
 }
